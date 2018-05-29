d242edb68e9b0c6f4805a3dc6c4b0bace13a5012
tnirmalz <37078408+tnirmalz@users.noreply.github.com>
2018-05-29 20:53:52 +0545

    Updated dig-dug.sh with live and dead hosts output
    An if else statement was added to check for live and dead hosts and then save the output accordingly

--- dig-dug.sh
+++ dig-dug.sh
@@ -14,15 +14,25 @@ sleep=0.3
 # Don't read lines with 'for'. Use a 'while' loop and 'read'.
 while IFS= read -r domain
 do
-  sleep $sleep
-  echo $domain 
-# Using +short to avoid auxiliary information.
-  ipaddress=`dig $domain +short` 
-  nameserver=`dig ns $domain +short`
-# Using 'tr' to replace new lines with commas, and strip horizontal whitespace.
-# Now with commas as delimiters, software can convert results to spreadsheet.
-  ipaddress_space=`echo -e "$ipaddress" | tr '\n' ',' | tr -d "[:blank:]"`
-  nameserver_space=`echo -e "$nameserver" | tr '\n' ',' | tr -d "[:blank:]"`
-# Outputting to the filename output.csv
-  echo -e "$domain,$ipaddress_space$nameserver_space" >> output.csv
+	sleep $sleep 
+	# Using +short to avoid auxiliary information.
+	ipaddress=`dig $domain +short` 
+	nameserver=`dig ns $domain +short`
+	# Using 'tr' to replace new lines with commas, and strip horizontal whitespace.
+	# Now with commas as delimiters, software can convert results to spreadsheet.
+	ipaddress_space=`echo -e "$ipaddress" | tr '\n' ',' | tr -d "[:blank:]"`
+	nameserver_space=`echo -e "$nameserver" | tr '\n' ',' | tr -d "[:blank:]"`
+
+	#check for live hosts
+	if [ "$ipaddress_space" == "," ] 
+	then
+		echo "$domain --> [!]Dead host[!]"
+		echo -e "$domain" >> dead_hosts.txt #if host is dead then save it to dead_hosts.txt
+
+	else
+		echo "$domain --> [~]Live host[~]"
+		# if host is live then make a csv output and domain only output
+		echo -e "$domain,$ipaddress_space$nameserver_space" >> output.csv
+		echo -e "$domain" >> live_hosts.txt	
+	fi
 done < "$1"
